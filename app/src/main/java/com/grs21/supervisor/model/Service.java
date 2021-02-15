3ed25f8d54f438c224e36c38ebc7c7c81d65ba56
Fatih Gürses <fatihgurses@gmail.com>
2021-02-15 20:20:54 +0300

    Service_Search_Item

--- app/src/main/java/com/grs21/supervisor/model/Service.java
+++ app/src/main/java/com/grs21/supervisor/model/Service.java
@@ -2,7 +2,60 @@ package com.grs21.supervisor.model;
 
 public class Service {
 
+    private String well;
+    private String elevatorUp;
+    private String machineRoom;
     private String date;
     private String employee;
 
+    public Service(String well, String elevatorUp, String machineRoom, String date, String employee) {
+        this.well = well;
+        this.elevatorUp = elevatorUp;
+        this.machineRoom = machineRoom;
+        this.date = date;
+        this.employee = employee;
+    }
+
+    public Service() {
+    }
+
+    public String getWell() {
+        return well;
+    }
+
+    public void setWell(String well) {
+        this.well = well;
+    }
+
+    public String getElevatorUp() {
+        return elevatorUp;
+    }
+
+    public void setElevatorUp(String elevatorUp) {
+        this.elevatorUp = elevatorUp;
+    }
+
+    public String getMachineRoom() {
+        return machineRoom;
+    }
+
+    public void setMachineRoom(String machineRoom) {
+        this.machineRoom = machineRoom;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(String employee) {
+        this.employee = employee;
+    }
 }
