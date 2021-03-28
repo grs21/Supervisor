078193e49bd995d2c1fb5a55a1132260292c0b38
Fatih Gürses <fatihgurses@gmail.com>
2021-03-28 18:49:55 +0300

    RecyclerView SeachView design

--- app/src/main/java/com/grs21/supervisor/adminFragment/ApartmentFragment.java
+++ app/src/main/java/com/grs21/supervisor/adminFragment/ApartmentFragment.java
@@ -2,11 +2,14 @@ package com.grs21.supervisor.adminFragment;
 
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -25,13 +28,12 @@ import com.grs21.supervisor.adapter.AdapterApartmentRecyclerView;
 import com.grs21.supervisor.databinding.FragmentAdminApartmentBinding;
 import com.grs21.supervisor.model.Apartment;
 import com.grs21.supervisor.model.User;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import es.dmoral.toasty.Toasty;
 
-public class ApartmentFragment extends Fragment implements SearchView.OnQueryTextListener {
+public class ApartmentFragment extends Fragment {
     private static final String TAG = "ApartmentFragment";
 
     private FragmentAdminApartmentBinding binding;
@@ -45,64 +47,67 @@ public class ApartmentFragment extends Fragment implements SearchView.OnQueryTex
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         binding=FragmentAdminApartmentBinding.inflate(inflater,container,false);
-
+        setHasOptionsMenu(true);
         Bundle bundle=getArguments();
         currentUser=(User) bundle.getSerializable("currentUser");
         fireStore=FirebaseFirestore.getInstance();
         getDataFromFireStore();
-        SearchView searchView= binding.searchView;
-        searchView.setOnQueryTextListener(this);
-
         return binding.getRoot();
     }
 
-    private void getDataFromFireStore() {
-        CollectionReference collectionReference=fireStore.collection(currentUser.getCompany());
-        collectionReference.orderBy("dateOfContract", Query.Direction.DESCENDING)
-                .addSnapshotListener(new EventListener<QuerySnapshot>() {
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        inflater.inflate(R.menu.builds_search_menu, menu);
+        MenuItem menuItem=menu.findItem(R.id.menuItemSearch);
+        SearchView searchView=(SearchView) menuItem.getActionView();
+        searchView.setImeOptions(EditorInfo.IME_ACTION_SEARCH);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
-            public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
-                if (error!=null){
-                    Toast toastSuccess= Toasty.success(getActivity(), R.string.saved
-                            ,Toast.LENGTH_SHORT,true);
-                    toastSuccess.setGravity(Gravity.CENTER, 0, 0);
-                    toastSuccess.show();
-                }
-                if (value!=null){
-                    apartments.clear();
-                    for (DocumentSnapshot snapshot:value.getDocuments()){
-                        Map<String,Object> getData=snapshot.getData();
-                        Apartment apartment=new Apartment( snapshot.getId(),(String)getData.get("buildName")
-                                ,(String)getData.get("address"),(String)getData.get("cost"),(String)getData.get("managerName")
-                                ,(String)getData.get("managerNumber"),(String)getData.get("managerAddress")
-                                ,(String)getData.get("employeeName"),(String)getData.get("employeeNumber")
-                                ,(String)getData.get("dateOfContract"),(String) getData.get("wellQRCOdeInfo")
-                                ,(String) getData.get("elevatorUpQRCOdeInfo"),(String) getData.get("machineQRCOdeInfo")
-                        , (ArrayList<HashMap>) getData.get("service"),(ArrayList<String>)getData.get("qrCodes"));
-                        apartments.add(apartment);
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
 
-                    }
-
-                    LinearLayoutManager linearLayoutManager=new LinearLayoutManager(getActivity());
-                    adapter=new AdapterApartmentRecyclerView(apartments,currentUser);
-                    binding.recyclerView.setLayoutManager(linearLayoutManager);
-                    binding.recyclerView.setAdapter(adapter);
-                    adapter.notifyDataSetChanged();
-                }
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                adapter.getFilter().filter(newText);
+                return false;
             }
         });
+        super.onCreateOptionsMenu(menu, inflater);
     }
 
-
-
-    @Override
-    public boolean onQueryTextSubmit(String query) {
-        return false;
-    }
-
-    @Override
-    public boolean onQueryTextChange(String newText) {
-        adapter.getFilter().filter(newText);
-        return false;
+    private void getDataFromFireStore() {
+        CollectionReference collectionReference = fireStore.collection(currentUser.getCompany());
+        collectionReference.orderBy("dateOfContract", Query.Direction.DESCENDING)
+            .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                @Override
+                public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                    if (error != null) {
+                        Toast toastSuccess = Toasty.success(getActivity(), R.string.saved
+                                , Toast.LENGTH_SHORT, true);
+                        toastSuccess.setGravity(Gravity.CENTER, 0, 0);
+                        toastSuccess.show();
+                    }
+                    if (value != null) {
+                        apartments.clear();
+                        for (DocumentSnapshot snapshot : value.getDocuments()) {
+                            Map<String, Object> getData = snapshot.getData();
+                            Apartment apartment = new Apartment(snapshot.getId(), (String) getData.get("buildName")
+                                    , (String) getData.get("address"), (String) getData.get("cost"), (String) getData.get("managerName")
+                                    , (String) getData.get("managerNumber"), (String) getData.get("managerAddress")
+                                    , (String) getData.get("employeeName"), (String) getData.get("employeeNumber")
+                                    , (String) getData.get("dateOfContract"), (String) getData.get("wellQRCOdeInfo")
+                                    , (String) getData.get("elevatorUpQRCOdeInfo"), (String) getData.get("machineQRCOdeInfo")
+                                    , (ArrayList<HashMap>) getData.get("service"), (ArrayList<String>) getData.get("qrCodes"));
+                            apartments.add(apartment);
+                        }
+                        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
+                        adapter = new AdapterApartmentRecyclerView(apartments, currentUser);
+                        binding.recyclerView.setLayoutManager(linearLayoutManager);
+                        binding.recyclerView.setAdapter(adapter);
+                        adapter.notifyDataSetChanged();
+                    }
+                }
+            });
     }
 }
--- app/src/main/java/com/grs21/supervisor/userFragment/UserApartmentFragment.java
+++ app/src/main/java/com/grs21/supervisor/userFragment/UserApartmentFragment.java
@@ -1,17 +1,18 @@
 package com.grs21.supervisor.userFragment;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.view.inputmethod.EditorInfo;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
-
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
@@ -19,17 +20,17 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.grs21.supervisor.R;
 import com.grs21.supervisor.adapter.AdapterUserApartmentRecyclerView;
 import com.grs21.supervisor.databinding.FragmentUserApartmentBinding;
 import com.grs21.supervisor.model.Apartment;
 import com.grs21.supervisor.model.User;
 import com.grs21.supervisor.util.ToastMessage;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
-public class UserApartmentFragment extends Fragment implements SearchView.OnQueryTextListener {
+public class UserApartmentFragment extends Fragment {
     private FragmentUserApartmentBinding binding;
     private FirebaseFirestore fireStore =FirebaseFirestore.getInstance();
     private User currentUser;
@@ -43,15 +44,33 @@ public class UserApartmentFragment extends Fragment implements SearchView.OnQuer
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-     binding=FragmentUserApartmentBinding.inflate(inflater,container,false);
-
+      binding=FragmentUserApartmentBinding.inflate(inflater,container,false);
+      setHasOptionsMenu(true);
       toastMessage=new ToastMessage();
       bundle=getArguments();
       currentUser=(User) bundle.getSerializable(bundleKeyCurrentUser);
       getDataFromFireStore();
-      SearchView searchView=binding.searchViewUserApartment;
-      searchView.setOnQueryTextListener(this);
      return binding.getRoot();
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        inflater.inflate(R.menu.builds_search_menu, menu);
+        MenuItem menuItem=menu.findItem(R.id.menuItemSearch);
+        SearchView searchView=(SearchView) menuItem.getActionView();
+        searchView.setImeOptions(EditorInfo.IME_ACTION_SEARCH);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                adapter.getFilter().filter(newText);
+                return false;
+            }
+        });
+        super.onCreateOptionsMenu(menu, inflater);
     }
 
     private void getDataFromFireStore() {
@@ -60,41 +79,29 @@ public class UserApartmentFragment extends Fragment implements SearchView.OnQuer
         .addSnapshotListener(new EventListener<QuerySnapshot>() {
             @Override
             public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
-            if (error!=null){
-                toastMessage.warningMessage(error.getMessage(), getContext());
-            }
-            if (value!=null){
-                for (DocumentSnapshot snapshot:value.getDocuments()){
-                    Map<String,Object> getData=snapshot.getData();
-                    Apartment apartment=new Apartment( snapshot.getId(),(String)getData.get("buildName")
-                    ,(String)getData.get("address"),(String)getData.get("cost"),(String)getData.get("managerName")
-                    ,(String)getData.get("managerNumber"),(String)getData.get("managerAddress")
-                    ,(String)getData.get("employeeName"),(String)getData.get("employeeNumber")
-                    ,(String)getData.get("dateOfContract"),(String) getData.get("wellQRCOdeInfo")
-                    ,(String) getData.get("elevatorUpQRCOdeInfo"),(String) getData.get("machineQRCOdeInfo")
-                    ,(ArrayList<HashMap>) getData.get("service") ,(ArrayList<String>)getData.get("qrCodes"));
-                    apartments.add(apartment);
-                }
-               LinearLayoutManager linearLayoutManager=new LinearLayoutManager(getContext());
-                adapter=new AdapterUserApartmentRecyclerView(apartments,currentUser);
-                binding.recyclerViewUserApartment.setAdapter(adapter);
-                binding.recyclerViewUserApartment.setLayoutManager(linearLayoutManager);
-                adapter.notifyDataSetChanged();
-            }
-
+                  if (error!=null){
+                      toastMessage.warningMessage(error.getMessage(), getContext());
+                  }
+                 if (value!=null){
+                     for (DocumentSnapshot snapshot:value.getDocuments()){
+                          Map<String,Object> getData=snapshot.getData();
+                          Apartment apartment=new Apartment( snapshot.getId(),(String)getData.get("buildName")
+                         ,(String)getData.get("address"),(String)getData.get("cost"),(String)getData.get("managerName")
+                         ,(String)getData.get("managerNumber"),(String)getData.get("managerAddress")
+                         ,(String)getData.get("employeeName"),(String)getData.get("employeeNumber")
+                         ,(String)getData.get("dateOfContract"),(String) getData.get("wellQRCOdeInfo")
+                         ,(String) getData.get("elevatorUpQRCOdeInfo"),(String) getData.get("machineQRCOdeInfo")
+                         ,(ArrayList<HashMap>) getData.get("service") ,(ArrayList<String>)getData.get("qrCodes"));
+                         apartments.add(apartment);
+                     }
+                     LinearLayoutManager linearLayoutManager=new LinearLayoutManager(getContext());
+                     adapter=new AdapterUserApartmentRecyclerView(apartments,currentUser);
+                     binding.recyclerViewUserApartment.setAdapter(adapter);
+                     binding.recyclerViewUserApartment.setLayoutManager(linearLayoutManager);
+                     adapter.notifyDataSetChanged();
+                 }
             }
         });
-
     }
 
-    @Override
-    public boolean onQueryTextSubmit(String query) {
-        return false;
-    }
-
-    @Override
-    public boolean onQueryTextChange(String newText) {
-        adapter.getFilter().filter(newText);
-        return false;
-    }
 }
